7ff53ce8196a7c96e09d68ae914757f05ed8cde6
Vic-Winderix <vicwinderix@proton.me>
2025-12-16 01:06:17 +0100

    Lambda Role

---
+++ AWS-Terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "uploads" {
+  bucket = "terraform-vicwin-uploads"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_s3_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
