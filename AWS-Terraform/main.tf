14417b973b7285ba571d8d207b94d3f009f190b0
Vic-Winderix <vicwinderix@proton.me>
2025-12-16 01:23:32 +0100

    Lambda and S3 upload detection

--- AWS-Terraform/main.tf
+++ AWS-Terraform/main.tf
@@ -1,3 +1,4 @@
+# Definieer de provider (AWS)
 terraform {
   required_providers {
     aws = {
@@ -7,32 +8,55 @@ terraform {
   }
 }
 
+# Stel de region in 
 provider "aws" {
   region = "eu-west-1"
 }
 
+# Maak de S3 bucket
 resource "aws_s3_bucket" "uploads" {
-  bucket = "terraform-vicwin-uploads"
-  acl    = "private"
+  bucket = "terraform-vicwin-uploads" # Naam van de bucket
+  acl    = "private" # Bucket zal private zijn
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_s3_role"
-  assume_role_policy = jsonencode({
+# Maak de lambda S3 role aan
+resource "aws_iam_role" "lambda_S3_role" { 
+  name = "lambda_s3_role" # Naam van de rol
+  assume_role_policy = jsonencode({ # Gebruik json code 
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
+      Action = "sts:AssumeRole" # Geef toestemming om de role te mogen gebruiken
       Effect = "Allow"
       Principal = {
-        Service = "lambda.amazonaws.com"
+        Service = "lambda.amazonaws.com" # Alleen lambda van AWS kan deze role aannemen
       }
     }]
   })
 }
 
+# Koppel IAM policy aan de rol
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_S3_role.name # Naam moet dezelfde zijn als hierboven vernoemd
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole" # Geef deze policy om logging mogelijk te maken
 }
 
-# Dit is een test aanpassing
+# S3 upload detectie
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.uploads.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.process_upload.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+
+# Geef toestemming aan S3 om lambda functie op te roepen
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.process_upload.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.uploads.arn
+}
